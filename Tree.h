966608a7fbb82d540309684f01008277c6a1bb1b
mirpapur <mrpepperbest@yandex.ru>
2020-05-24 00:41:04 +0300

    Add supa f'n test!

--- Tree.h
+++ Tree.h
@@ -2,6 +2,7 @@
 #ifndef TREE_H
 #define TREE_H
 #include "Timer.h"
+#include <fstream>
 #include "AVL_Node.h"
 #include <iostream>
 #include <memory>
@@ -9,6 +10,7 @@
 #include <functional>
 #include <utility>
 #include <algorithm>
+#include <iomanip>
 using namespace std;
 template <typename T>
 class Tree {
@@ -19,15 +21,18 @@ public:
     virtual Node<T>* FIND_NODE(T data){};                    //search with key
     virtual void INSERT(T data){};                           //insertion
     virtual void DELETE(T data){};                           //deleting
-//interface tests
-    virtual void TESTING_INSERT(int n) final;
-    virtual void TESTING_DELETING(int n) final;
-    virtual void TESTING_MAX_ELEMENT(int n) final;
-    virtual void TESTING_MIN_ELEMENT(int n) final;
-    virtual void TESTING_FINDING_ELEMENT(int n) final;
+//interface tests //enter amount of elements for every test
+    virtual double TESTING_INSERT(int n) final;
+    virtual double TESTING_DELETING(int n) final;
+    virtual double TESTING_MAX_ELEMENT(int n) final;
+    virtual double TESTING_MIN_ELEMENT(int n) final;
+    virtual double TESTING_FINDING_ELEMENT(int n) final;
+//interface output of testing data
+    virtual void MEGA_BANG(int start,int step, int cycles) final; //the biggest *ucking test ever with output!
+    //first input starting number of elements, then amount of adding elements pers step, then amount of these steps.
 };
 //test funcs declaration
-template <typename T> void Tree<T>::TESTING_INSERT(int n)
+template <typename T>double Tree<T>::TESTING_INSERT(int n)
 {
     Timer t;
     for(int i=0; i<n; i++) {
@@ -37,9 +42,10 @@ template <typename T> void Tree<T>::TESTING_INSERT(int n)
     Timer a;
     INSERT(n);
     cout << "Time of inserting of 1 element into the tree of " << n << " elements is " << a.elapsed() << " ms " << endl<<endl;
+    return a.elapsed();
 }         //insertion test
 
-template <typename T>void Tree<T>::TESTING_DELETING(int n)
+template <typename T>double Tree<T>::TESTING_DELETING(int n)
 {
     for(int i=0; i<n; i++) {
         INSERT(i);
@@ -48,9 +54,10 @@ template <typename T>void Tree<T>::TESTING_DELETING(int n)
     INSERT(n);
     DELETE(n-1);
     cout << "Time of deleting of an element is " << a.elapsed()  << "ms" << endl<<endl;
+    return a.elapsed();
 }        //deleting test
 
-template <typename T>void Tree<T>::TESTING_MAX_ELEMENT(int n)
+template <typename T>double Tree<T>::TESTING_MAX_ELEMENT(int n)
 {
     unsigned int start_time =  clock();
     for(int i=0; i<n; i++) {
@@ -59,9 +66,10 @@ template <typename T>void Tree<T>::TESTING_MAX_ELEMENT(int n)
     Timer a;
     FIND_MAX_ELEMENT();
     cout << "Time of finding of a needed element in a tree of " << n << " elements is " <<  a.elapsed() << "s"<< endl;
+    return a.elapsed();
 }     //max test
 
-template <typename T>void Tree<T>::TESTING_MIN_ELEMENT(int n)
+template <typename T>double Tree<T>::TESTING_MIN_ELEMENT(int n)
 {
     unsigned int start_time =  clock();
     for(int i=0; i<n; i++) {
@@ -70,9 +78,10 @@ template <typename T>void Tree<T>::TESTING_MIN_ELEMENT(int n)
     Timer a;
     FIND_MIN_ELEMENT();
     cout << "Time of finding of a needed element in a tree of " << n << " elements is " <<  a.elapsed() << "s"<< endl;
+    return a.elapsed();
 }     //min test
 
-template <typename T>void Tree<T>::TESTING_FINDING_ELEMENT(int n)
+template <typename T>double Tree<T>::TESTING_FINDING_ELEMENT(int n)
 {
     unsigned int start_time =  clock();
     for(int i=0; i<n; i++) {
@@ -81,5 +90,20 @@ template <typename T>void Tree<T>::TESTING_FINDING_ELEMENT(int n)
     Timer a;
     FIND_NODE(n-1);
     cout << "Time of finding of a needed element in a tree of " << n << " elements is " <<  a.elapsed() << "s"<< endl;
+    return a.elapsed();
 } //search test
+
+template <typename T>void Tree<T>::MEGA_BANG(int start,int step, int cycles){
+    ofstream fout("info.dat");
+    cout<< "insert delete min max search"<<endl;
+    fout<< "insert delete min max search"<<endl;
+    for(int i=start;i<(start+step*cycles);i+=step) {
+        fout << endl << setprecision(10) << this->TESTING_INSERT(i) << " "           //insert output
+             << this->TESTING_DELETING(i) << " "                                        //delete output
+             << this->TESTING_MAX_ELEMENT(i) << " "                                     //max output
+             << this->TESTING_MIN_ELEMENT(i) << " "                                     //min output
+             << this->TESTING_FINDING_ELEMENT(i);                                       //searching output
+    };
+    fout.close();
+} //total checkout! outputting into "info.dat"
 #endif //TREE_H
